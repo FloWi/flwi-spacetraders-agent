e79ebba8db58ef53c6ec2237ab77e4e4d057ccf6
Florian Witteler <github@flwi.de>
2025-03-09 18:42:34 +0100

    ships now render nicely

--- st-server/src/ship_overview_page.rs
+++ st-server/src/ship_overview_page.rs
@@ -8,10 +8,12 @@ use leptos::*;
 use leptos::{component, view, IntoView};
 use leptos_use::{use_interval, UseIntervalReturn};
 use serde::{Deserialize, Serialize};
-use st_domain::{Ship, ShipSymbol, StStatusResponse};
+use st_domain::{FlightMode, Ship, ShipSymbol, StStatusResponse};
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::time::Duration;
+use leptos::attr::height;
+use phosphor_leptos::{Icon, IconWeight, TRUCK, ROCKET, AIRPLANE_LANDING, AIRPLANE_TAKEOFF, GAS_PUMP, GAS_CAN, PACKAGE};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ShipsOverview {
@@ -50,6 +52,49 @@ async fn get_ships_overview(get_ships_mode: GetShipsMode) -> Result<ShipsOvervie
 }
 
 #[component]
+pub fn ShipCard<'a>(ship: &'a Ship) -> impl IntoView {
+
+    let is_traveling = match ship.nav.flight_mode {
+        FlightMode::Cruise => true,
+        FlightMode::Drift => false,
+        FlightMode::Stealth => false,
+        FlightMode::Burn => false
+    };
+
+    let fuel_str = format!("{} / {}", ship.fuel.current, ship.fuel.capacity,);
+    let cargo_str = format!("{} / {}", ship.cargo.units, ship.cargo.capacity,);
+
+    view! {
+        <div class="p-3 border-4 border-blue-900 text-slate-400">
+            <div class="flex flex-row gap-4">
+                <Icon icon=TRUCK size="3em" />
+                <div class="flex flex-col gap-1">
+                    <h3 class="text-xl text-white">{format!("{}", &ship.symbol.0)}</h3>
+                    <p class="text-slate-400">"Role"</p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-1">
+                <div class="flex flex-row gap-2">
+                    <Icon icon=TRUCK />
+                    <p>{format!("Location: {}", &ship.nav.waypoint_symbol.0)}</p>
+                </div>
+                <div class="flex flex-row items-center gap-2">
+                    <div class="flex flex-row items-center gap-1">
+                        <Icon icon=GAS_PUMP />
+                        <p>{fuel_str}</p>
+                    </div>
+                    <div class="flex flex-row items-center gap-1">
+                        <Icon icon=PACKAGE />
+                        <p>{cargo_str}</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    }
+}
+
+#[component]
 pub fn ShipOverviewPage() -> impl IntoView {
     let UseIntervalReturn {
         counter,
@@ -65,7 +110,7 @@ pub fn ShipOverviewPage() -> impl IntoView {
     );
 
     view! {
-        <div>
+        <div class="bg-blue-950 text-white flex flex-col min-h-screen">
             <h1 class="font-bold text-2xl">"Ships Status"</h1>
             <div>
                 <Suspense>
@@ -76,25 +121,17 @@ pub fn ShipOverviewPage() -> impl IntoView {
                                 view! {
                                     <div class="flex flex-col gap-4 p-4">
                                         <h2 class="font-bold text-xl">
-                                            {format!(
-                                                "Fleet has {} ships",
-                                                ships_overview.ships.len(),
-                                            )}
+                                            {format!("Fleet has {} ships", ships_overview.ships.len())}
                                         </h2>
-                                        <p>{format!("Last Update: {:?}", ships_overview.last_update)}</p>
-                                        <div class="flex flex-wrap">
+                                        <p>
+                                            {format!("Last Update: {:?}", ships_overview.last_update)}
+                                        </p>
+                                        <div class="flex flex-wrap gap-2">
                                             {ships_overview
                                                 .ships
                                                 .iter()
                                                 .map(|ship| {
-                                                    view! {
-                                                        <div>
-                                                            <h3>{format!("{}", &ship.symbol.0)}</h3>
-                                                            <p>
-                                                                {format!("Location: {}", &ship.nav.waypoint_symbol.0)}
-                                                            </p>
-                                                        </div>
-                                                    }
+                                                    view! { <ShipCard ship=ship /> }
                                                 })
                                                 .collect_view()}
                                         </div>
